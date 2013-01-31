d58ad6944c8d3e00cec43badec69342cd2bc31ee
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-01-31 14:37:19 +0100

    Update indentation

--- src/com/apibootstraper/mobile/core/Todo.java
+++ src/com/apibootstraper/mobile/core/Todo.java
@@ -1,138 +1,144 @@
 package com.apibootstraper.mobile.core;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.apibootstraper.mobile.core.util.DateUtils;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.apibootstraper.mobile.core.util.HTTPClient;
+import com.apibootstraper.mobile.core.util.HTTPResponse;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class Todo implements Serializable {
 
-	/** serialVersionUID */
-	private static final long serialVersionUID = -2851331358231023655L;
+    /** serialVersionUID */
+    private static final long serialVersionUID = -2851331358231023655L;
 
-	private String uuid;
+    private String uuid;
 
-	private Date createdAt;
+    private Date createdAt;
 
-	private Date updatedAt;
+    private Date updatedAt;
 
-	private User user;
+    private User user;
 
-	/**
-	 * @return id
-	 */
-	public String getUUID() {
-		return uuid;
-	}
+    /**
+     * @return id
+     */
+    public String getUUID() {
+        return uuid;
+    }
 
-	/**
-	 * @param id
-	 * @return this todo
-	 */
-	public Todo setUUID(String uuid) {
-		this.uuid = uuid;
-		return this;
-	}
+    /**
+     * @param id
+     * @return this todo
+     */
+    public Todo setUUID(String uuid) {
+        this.uuid = uuid;
+        return this;
+    }
 
-	/**
-	 * @return createdAt
-	 */
-	public Date getCreatedAt() {
-		return DateUtils.clone(createdAt);
-	}
+    /**
+     * @return createdAt
+     */
+    public Date getCreatedAt() {
+        return DateUtils.clone(createdAt);
+    }
 
-	/**
-	 * @param createdAt
-	 * @return this todo
-	 */
-	public Todo setCreatedAt(Date createdAt) {
-		this.createdAt = DateUtils.clone(createdAt);
-		return this;
-	}
+    /**
+     * @param createdAt
+     * @return this todo
+     */
+    public Todo setCreatedAt(Date createdAt) {
+        this.createdAt = DateUtils.clone(createdAt);
+        return this;
+    }
 
-	/**
-	 * @return updatedAt
-	 */
-	public Date getUpdatedAt() {
-		return DateUtils.clone(updatedAt);
-	}
+    /**
+     * @return updatedAt
+     */
+    public Date getUpdatedAt() {
+        return DateUtils.clone(updatedAt);
+    }
 
-	/**
-	 * @param updatedAt
-	 * @return this todo
-	 */
-	public Todo setUpdatedAt(Date updatedAt) {
-		this.updatedAt = DateUtils.clone(updatedAt);
-		return this;
-	}
+    /**
+     * @param updatedAt
+     * @return this todo
+     */
+    public Todo setUpdatedAt(Date updatedAt) {
+        this.updatedAt = DateUtils.clone(updatedAt);
+        return this;
+    }
 
-	/**
-	 * @return user
-	 */
-	public User getUser() {
-		return user;
-	}
+    /**
+     * @return user
+     */
+    public User getUser() {
+        return user;
+    }
 
-	/**
-	 * @param user
-	 * @return this todo
-	 */
-	public Todo setUser(User user) {
-		this.user = user;
-		return this;
-	}
+    /**
+     * @param user
+     * @return this todo
+     */
+    public Todo setUser(User user) {
+        this.user = user;
+        return this;
+    }
 
-	/**
-	 * Find all todo
-	 * 
-	 * @param uuid
-	 * @param responseHandler
-	 */
-	public static void findAll(AsyncHttpResponseHandler responseHandler) {
-		HTTPClient.getInstance().get("todo", null, new JsonHttpResponseHandler() {
+    /**
+     * Find all todo
+     * 
+     * @param uuid
+     * @param responseHandler
+     */
+    public static void findAll(final HTTPResponse<ArrayList<Todo>> response) {
+        HTTPClient.getInstance().get("todo", null, new JsonHttpResponseHandler() {
 
-			@Override
-			public void onSuccess(JSONArray timeline) {
-				try {
-					JSONObject firstEvent = (JSONObject)timeline.get(0);
-					String tweetText = firstEvent.getString("text");
+            @Override
+            public void onSuccess(JSONArray todos) {
+                try {
+                    ArrayList<Todo> todoList = new ArrayList<Todo>();
 
-					// Do something with the response
-					System.out.println(tweetText);
-				} catch(JSONException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//                    for (JSONObject t : todos) {
+//                        Todo todo = new Todo();
+//                        todoList.add(todo);
+//                    }
 
-	/**
-	 * Find a todo by UUID
-	 * 
-	 * @param uuid
-	 * @param responseHandler
-	 */
-	public static void findByUUID(String uuid, AsyncHttpResponseHandler responseHandler) {
-		HTTPClient.getInstance().get("todo/" + uuid, null, new JsonHttpResponseHandler() {
+                    response.onSuccess(todoList);
 
-			@Override
-			public void onSuccess(JSONArray timeline) {
-				try {
-					JSONObject firstEvent = (JSONObject)timeline.get(0);
-					String tweetText = firstEvent.getString("text");
+                } catch(Exception e) {
+                    e.printStackTrace();
 
-					// Do something with the response
-					System.out.println(tweetText);
-				} catch(JSONException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+                    response.onFailure(e, null);
+                }
+            }
+        });
+    }
+
+    /**
+     * Find a todo by UUID
+     * 
+     * @param uuid
+     * @param responseHandler
+     */
+    public static void findByUUID(String uuid, final HTTPResponse<Todo> response) {
+        HTTPClient.getInstance().get("todo/" + uuid, null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(JSONArray timeline) {
+                try {
+                    Todo todo = new Todo();
+
+                    response.onSuccess(todo);
+                } catch(Exception e) {
+                    e.printStackTrace();
+
+                    response.onFailure(e, null);
+                }
+            }
+        });
+    }
 }
--- src/com/apibootstraper/mobile/core/User.java
+++ src/com/apibootstraper/mobile/core/User.java
@@ -4,93 +4,103 @@ import java.io.Serializable;
 import java.util.Date;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.apibootstraper.mobile.core.util.DateUtils;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.apibootstraper.mobile.core.util.HTTPClient;
+import com.apibootstraper.mobile.core.util.HTTPResponse;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class User implements Serializable {
 
-	/** serialVersionUID */
-	private static final long serialVersionUID = -4670218264175191002L;
+    /** serialVersionUID */
+    private static final long serialVersionUID = -4670218264175191002L;
 
-	private String uuid;
+    private String uuid;
 
-	private Date createdAt;
+    private Date createdAt;
 
-	private Date updatedAt;
+    private Date updatedAt;
 
-	/**
-	 * @return id
-	 */
-	public String getUUID() {
-		return uuid;
-	}
+    /**
+     * @return id
+     */
+    public String getUUID() {
+        return uuid;
+    }
 
-	/**
-	 * @param id
-	 * @return this user
-	 */
-	public User setUUID(String uuid) {
-		this.uuid = uuid;
-		return this;
-	}
+    /**
+     * @param id
+     * @return this user
+     */
+    public User setUUID(String uuid) {
+        this.uuid = uuid;
+        return this;
+    }
 
-	/**
-	 * @return createdAt
-	 */
-	public Date getCreatedAt() {
-		return DateUtils.clone(createdAt);
-	}
+    /**
+     * @return createdAt
+     */
+    public Date getCreatedAt() {
+        return DateUtils.clone(createdAt);
+    }
 
-	/**
-	 * @param createdAt
-	 * @return this User
-	 */
-	public User setCreatedAt(Date createdAt) {
-		this.createdAt = DateUtils.clone(createdAt);
-		return this;
-	}
+    /**
+     * @param createdAt
+     * @return this User
+     */
+    public User setCreatedAt(Date createdAt) {
+        this.createdAt = DateUtils.clone(createdAt);
+        return this;
+    }
 
-	/**
-	 * @return updatedAt
-	 */
-	public Date getUpdatedAt() {
-		return DateUtils.clone(updatedAt);
-	}
+    /**
+     * @return updatedAt
+     */
+    public Date getUpdatedAt() {
+        return DateUtils.clone(updatedAt);
+    }
 
-	/**
-	 * @param updatedAt
-	 * @return this user
-	 */
-	public User setUpdatedAt(Date updatedAt) {
-		this.updatedAt = DateUtils.clone(updatedAt);
-		return this;
-	}
+    /**
+     * @param updatedAt
+     * @return this user
+     */
+    public User setUpdatedAt(Date updatedAt) {
+        this.updatedAt = DateUtils.clone(updatedAt);
+        return this;
+    }
 
-	/**
-	 * Find a user by UUID
-	 * 
-	 * @param uuid
-	 * @param responseHandler
-	 */
-	public static void findByUUID(String uuid, AsyncHttpResponseHandler responseHandler) {
-		HTTPClient.getInstance().get("user/" + uuid, null, new JsonHttpResponseHandler() {
+    /**
+     * Find a user by UUID
+     * 
+     * @param uuid
+     * @param responseHandler
+     */
+    public static void findByUUID(String uuid, final HTTPResponse<User> response) {
+        HTTPClient.getInstance().get("user/" + uuid, null, new JsonHttpResponseHandler() {
 
-			@Override
-			public void onSuccess(JSONArray timeline) {
-				try {
-					JSONObject firstEvent = (JSONObject)timeline.get(0);
-					String tweetText = firstEvent.getString("text");
+            @Override
+            public void onSuccess(JSONArray timeline) {
+                try {
+                    User user= new User();
 
-					// Do something with the response
-					System.out.println(tweetText);
-				} catch(JSONException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+                    response.onSuccess(user);
+                } catch(Exception e) {
+                    e.printStackTrace();
+
+                    response.onFailure(e, null);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable e, String c) {
+                // TODO automatically pass the JsonHttpResponseHandler onFailure into HTTPResponse if not Override
+                response.onFailure(e, c);
+            }
+            
+            @Override
+            public void onFinish() {
+                response.onFinish();
+            }
+        });
+    }
 }
