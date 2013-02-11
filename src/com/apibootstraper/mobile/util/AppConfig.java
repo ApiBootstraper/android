2d81cb5bfd6d0b2464459b234262e937250fe71e
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-02-11 18:26:04 +0100

    Update AppConfig.java
    Change API App ID and Key

--- src/com/apibootstraper/mobile/util/AppConfig.java
+++ src/com/apibootstraper/mobile/util/AppConfig.java
@@ -5,7 +5,7 @@ public class AppConfig {
     public static final String HTTP_API_BASE_URL = "http://nameless-forest-4117.herokuapp.com/";
     public static final String HTTP_API_VERSION  = "1.0.0";
 
-    public static final String HTTP_API_APP_ID   = "OmDCHVNAOaWGJxSJ";
-    public static final String HTTP_API_APP_KEY  = "b4TrpLfwenHbJZlTjwWddwPwPJTMLZGF";
+    public static final String HTTP_API_APP_ID   = "uaYFUYNR3Vf0xmDo";
+    public static final String HTTP_API_APP_KEY  = "gKdNb8sf7sv3pNtzjq3oI8AzuemR9ind";
 
 }
